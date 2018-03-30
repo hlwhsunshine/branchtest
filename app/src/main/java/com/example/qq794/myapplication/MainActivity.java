4da6d450183b86eccf9325d04b3e70718c20db27
hlwhsunshine <qq794400859@outlook.com>
2018-03-30 13:58:35 +0800

    用于branch测试

---
+++ app/src/main/java/com/example/qq794/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.qq794.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CircleBar circleBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        circleBar = findViewById(R.id.cb);
+        circleBar.setDesText("进度");
+        circleBar.setText("9");
+        circleBar.setSweepAngle(100);
+    }
+}
